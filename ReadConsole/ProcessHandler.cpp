1fb0ad17ae1da7aef47aeabb215c6cc1fb669d81
Dashro <wirtzfabian@yahoo.de>
2015-11-20 13:13:02 +0100

    w

--- ReadConsole/ProcessHandler.cpp
+++ ReadConsole/ProcessHandler.cpp
@@ -6,12 +6,13 @@ CProcessHandler::CProcessHandler(QObject *parent)
 	//QString program = "C:/Users/Fabian/Documents/Tests/ConsolePrinter/Win32/Debug/ConsolePrinter.exe";
 	myProcess = new QProcess(this);
 
+
 	myProcess->setProcessChannelMode(QProcess::ForwardedChannels);
 
 	//connect(myProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(processOutput()));
 	//myProcess->execute(program);  
-	myProcess->setWorkingDirectory("/home/pi/SMA");
-	myProcess->execute("/home/pi/SMA/QuerySMA.sh");
+
+	myProcess->execute("/bin/sh", QStringList() << "/home/pi/SMA/QuerySMA.sh");
 
 
 	//if (!myProcess->waitForStarted())
